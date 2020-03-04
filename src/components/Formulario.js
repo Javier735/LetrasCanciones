be97bf9b3438384210e9b4a8921b91b07eaa1525
rodrix <rodrix.ja@gmail.com>
2020-03-04 17:23:59 -0600

    Agregando los state y la validacion

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () => {
+
+    const[busqueda,guardarBusqueda]=useState({
+        artista:'',
+        cancion:''
+    });
+    const [error, guardarError]=useState(false);
+
+    const {artista,cancion}=busqueda;
+    
+    //funcion a cada input para leer su contenido
+    const actualizarState = e =>{
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+
+        })
+    }
+
+    //consultar las apis
+    const buscarInformacion = e =>{
+        e.preventDefault();
+        if(artista.trim() ==='' || cancion.trim() ===''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        //todo bien pasar al componente principal
+
+    }
+
     return ( 
         <div className="bg-info">
+             {error ? <p className="alert alert-danger text-center p-2">Campos obligatorios</p>:null}
             <div className="container">
-                <div className="row">
+                <div className="row">                   
                     <form
+                        onSubmit={buscarInformacion}
                        className="col card text-white bg-transparent mb-5 pt-5 pb-2" 
                     >
                         <fieldset>
@@ -19,6 +51,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="artista"
                                             placeholder="Nombre del artista"
+                                            onChange={actualizarState}
+                                            value={artista}
                                         />
                                     </div>
                                 </div>
@@ -30,6 +64,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="cancion"
                                             placeholder="Nombre canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
                                         />
                                     </div>
                                 </div>
